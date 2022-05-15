80d81926fc92867a42ea5a8d7f5cd1df24aeebb5
haseHH <christian@hase.hamburg>
2022-05-15 13:28:25 +0200

    add function test-call script

---
+++ helpers/Invoke-FunctionCall.ps1
@@ -0,0 +1,23 @@
+$Function = 'new'
+
+$baseScriptPath = $PSScriptRoot
+if (($null -ne $psEditor) -and ([string]::IsNullOrEmpty($baseScriptPath))) {
+    $baseScriptPath = ([Io.FileInfo]$psEditor.GetEditorContext().CurrentFile.Path).Directory.FullName
+}
+
+$request = Get-Content -Path "${baseScriptPath}\..\azure-function\${Function}\sample.dat" -Raw
+$functionJson = Get-Content -Path "${baseScriptPath}\..\azure-function\${Function}\function.json" | ConvertFrom-Json
+$method = ($functionJson.bindings | Where-Object {$null -ne $_.methods} | Select-Object -ExpandProperty 'methods').ToUpper()
+$baseUri = 'http://localhost:7071/api'
+
+$callParams = @{
+    Method  = $method
+    Verbose = $true
+}
+if ($method -eq 'GET') {
+    $callParams.Add('Uri', "${baseUri}/${Function}?${request}")
+} else {
+    $callParams.Add('Uri', "${baseUri}/${Function}")
+    $callParams.Add('Body', $request)
+}
+Invoke-RestMethod @callParams
